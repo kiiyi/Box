7e2dcf76226aad0d28b0209c7f208518595a5016
kiiyi <kiiyi@pm.me>
2020-11-10 11:32:01 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+// scripts/index.js
+module.exports = async function main(callback) {
+    try {
+      // Set up a Truffle contract, representing our deployed Box instance
+      const Box = artifacts.require("Box");
+      const box = await Box.deployed();
+
+      // Send a transaction to store() a new value in the Box
+      await box.store(23);
+      
+      // Call the retrieve() function of the deployed Box contract
+      value = await box.retrieve();
+      console.log("Box value is", value.toString());
+
+      callback(0);
+    } catch (error) {
+      console.error(error);
+      callback(1);
+    }
+}
